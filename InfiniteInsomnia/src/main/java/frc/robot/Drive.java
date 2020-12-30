0514d7fb6bc56c28f7c4f7c011cbf37ccf61278f
MarekNemec <73655996+MarekNemec@users.noreply.github.com>
2020-12-30 22:01:23 +0100

    Update Drive.java

--- InfiniteInsomnia/src/main/java/frc/robot/Drive.java
+++ InfiniteInsomnia/src/main/java/frc/robot/Drive.java
@@ -33,11 +33,30 @@ public class Drive {
 
     //Proč je tady autodrive pouze na první 2 sekundy?
     public void autoDrive() {
-        if(timer.get() <= 2.00) {
-            robotMap.drive.driveCartesian(0.75, 0, 0);
-        } else {
-            robotMap.drive.driveCartesian(0, 0, 0);
+        //Jede doleva (a potom doprava, aby se mohl otočit)
+        if(timer.get() >= 0 AND < 0.5) {
+            robotMap.drive.driveCartesian(0, 0.75, 0);
+            while loop = Constants.Left;
         }
-    }
-
+        //Jede doprava
+        if(timer.get() >= 0.5)) {
+            robotMap.drive.driveCartesian(0.75, 0, 0);
+            while loop = Constants.Straight;
+        }
+        //Otočka o 45°, aby objel brick
+        if(timer.get() >= 1)) {
+            robotMap.drive.driveCartesian(0, 0, 0);
+            while loop = Constants.FirstTurn;
+        }
+        //Jede před Flask
+        if(timer.get() >= 1.5)) {
+            robotMap.drive.driveCartesian(0.75, 0, 0);
+            while loop = Constants.Straight;
+        }
+        //Otáčí se na střelbu o 135°
+        if(timer.get() >= 2)) {
+            robotMap.drive.driveCartesian(0, 0, 0);
+            while loop = Constants.SecondTurn;
+        }
+     }
 }
